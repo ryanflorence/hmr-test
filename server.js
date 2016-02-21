73ee0ebcb09a3706a2bf78099488d507b5cf2af9
Ryan Florence <rpflorence@gmail.com>
2016-02-21 11:12:20 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 /*
  * This file is simply replacing `webpack --inline --hot --content-base .`
- * When run from the CLI HMR works, but not when created with server :(
  */
 
 import WebpackDevServer from 'webpack-dev-server'
